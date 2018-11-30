b6082a66324bbee4768cdee5285e31a6dede8e7b
Daniel Lemes Gribel <gribel.daniel@gmail.com>
2018-11-30 15:28:08 -0500

    Removing MinAssignment files

--- MinAssignment.h
+++
@@ -1,9 +0,0 @@
-#ifndef MinAssignment_h
-#define MinAssignment_h
-
-#include "dlib-master/dlib/optimization/max_cost_assignment.h"
-#include <iostream>
-
-std::vector<long> minAssignment(double** mat, int m);
-
-#endif
